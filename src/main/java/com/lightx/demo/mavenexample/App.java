431a47f410b6e0e74a7a5849b4c2dd889709f9eb
santosh <santosh080305.ms@gmail.com>
2024-11-14 12:25:22 +0530

    SE MAVEN PROJECT

---
+++ src/main/java/com/lightx/demo/mavenexample/App.java
@@ -0,0 +1,13 @@
+package com.lightx.demo.mavenexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "M SANTOSH - 22BD1A0534" );
+    }
+}
